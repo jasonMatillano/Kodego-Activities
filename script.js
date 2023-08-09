c9c8bc0f5d2fc192641f43ce5287b8a9a744fc89
Jason <jason.matillano@gmail.com>
2023-08-09 13:40:50 +0800

    perfect, get things done" adsfasdf

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const winCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
 let currentPlayer = "X";
 let board = ["", "", "", "", "", "", "", "", ""];
 
@@ -5,38 +16,46 @@ function makeMove(cellIndex) {
   if (board[cellIndex] === "") {
     board[cellIndex] = currentPlayer;
     document.getElementsByClassName("cell")[cellIndex].innerText = currentPlayer;
-
-    currentPlayer = currentPlayer === "X" ? "O" : "X"; // Switch the current player
-
+    document.getElementsByClassName("cell")[cellIndex].classList.add("selected");
     if (checkWin(currentPlayer)) {
-      alert(currentPlayer + " wins!");
-      resetBoard();
+      highlightWinningCombination(currentPlayer);
+      setTimeout(function() {
+        alert(currentPlayer + " wins!");
+        resetBoard();
+      }, 200);
     } else if (board.filter(cell => cell === "").length === 0) {
       alert("It's a draw!");
       resetBoard();
+    } else {
+      currentPlayer = currentPlayer === "X" ? "O" : "X";
     }
   }
 }
 
 function checkWin(player) {
-  const winCombinations = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-
   return winCombinations.some(combination =>
     combination.every(index => board[index] === player)
   );
 }
 
+function highlightWinningCombination(player) {
+  winCombinations.forEach(combination => {
+    const [a, b, c] = combination;
+    if (board[a] === player && board[b] === player && board[c] === player) {
+      document.getElementsByClassName("cell")[a].classList.add("win");
+      document.getElementsByClassName("cell")[b].classList.add("win");
+      document.getElementsByClassName("cell")[c].classList.add("win");
+    }
+  });
+}
+
 function resetBoard() {
   currentPlayer = "X";
   board = ["", "", "", "", "", "", "", "", ""];
-  document.querySelectorAll(".cell").forEach(cell => (cell.innerText = ""));
-}   
+  const cells = document.getElementsByClassName("cell");
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].innerText = "";
+    cells[i].classList.remove("selected");
+    cells[i].classList.remove("win");
+  }
+}
