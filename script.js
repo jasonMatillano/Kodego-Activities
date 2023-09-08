737d6fc598e899947575e723e9362ca7be2a9ec5
Jason <mat.lopez072023@gmail.com>
2023-09-08 19:30:58 +0800

    while demo

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-function displayNamesAndCount(names) {
-  // Create an empty array of students
-  var students = []
+let i = 1
 
-  // Display each name using a for loop
-  for (let i = 0; i < names.length; i++) {
-    // console.log(names[i]);
-    students.push(names[i])
-  }
+while (i <= 10) {
 
-  // Display the total number of names in the array
-  console.log('Students names are:', students);
-  console.log('Total number of names:', names.length);  
+  // perform operation here
+  console.log(i)
+
+  i++
 }
-
-// Example usage:
-const namesArray = ['Alice', 'Bob', 'Charlie', 'David'];
-displayNamesAndCount(namesArray);
