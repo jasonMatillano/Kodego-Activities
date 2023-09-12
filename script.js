47d1225aca1721f18f3bcee863314af2fecd9959
Jason <mat.lopez072023@gmail.com>
2023-09-12 19:43:38 +0800

    CheckJoe

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
-let i = 1
+function isJohnIncluded(students) {
+  const targetName = "John";
+  let isFound = false;
+  
+  students.forEach(student => {
+    // Convert both names to lowercase and then compare
+    if (student.name.toLowerCase() === targetName.toLowerCase()) {
+      isFound = true; // John is found
+    }
+  });
+  
+  return isFound;
+}
 
-while (i <= 10) {
+const students = [
+  {
+    name: "Alice",
+    age: 20,
+    grade: 85,
+  },
+  {
+    name: "Bob",
+    age: 21,
+    grade: 92,
+  },
+  {
+    name: "Carol",
+    age: 19,
+    grade: 78,
+  },
+  {
+    name: "David",
+    age: 22,
+    grade: 95,
+  },
+  {
+    name: "Eva",
+    age: 20,
+    grade: 88,
+  },
+];
 
-  // perform operation here
-  console.log(i)
-
-  i++
+if (isJohnIncluded(students)) {
+  console.log("John is included in the list of students.");
+} else {
+  console.log("John is not included in the list of students.");
 }
