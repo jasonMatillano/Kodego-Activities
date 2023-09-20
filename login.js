ba5f453cfc8aa03ec8a2b558939aa9f2ec012953
Jason <mat.lopez072023@gmail.com>
2023-09-20 20:29:35 +0800

    0.2.4.DOM_Demo

---
+++ login.js
@@ -0,0 +1,47 @@
+var divElement = document.createElement('div');
+var buttonElement = document.createElement('button');
+
+divElement.id = 'tester-id';
+buttonElement.id = 'div-id';
+buttonElement.className = 'div-class';
+
+divElement.appendChild(buttonElement); 
+// gawa na yung unang container at yung button sa loob
+
+var parElement = document.createElement('p'); parElement.innerText = 'Im a paragraph';
+
+buttonElement.appendChild(parElement);
+
+// gawa na dito yung main objective;
+
+document.body.appendChild(divElement);
+
+
+
+// Function to add a new paragraph element to the container
+function addParagraph() {
+    const newParagraph = document.createElement('p');
+    newParagraph.textContent = 'New paragraph added!';
+    const container = document.getElementById('container');
+    container.appendChild(newParagraph);
+}
+
+// Function to modify the text of the first paragraph
+function modifyText() {
+    const container = document.getElementById('container');
+    const firstParagraph = container.querySelector('p');
+    if (firstParagraph) {
+        firstParagraph.textContent = 'Text modified!';
+    }
+}
+
+// Function to delete the first paragraph
+function deleteParagraph() {
+    const container = document.getElementById('container');
+    const firstParagraph = container.querySelector('p');
+    if (firstParagraph) {
+        container.removeChild(firstParagraph);
+    }
+}
+
+
