66c295a63f0f87860f138377756c60be5c92a823
Jason <mat.lopez072023@gmail.com>
2023-10-06 20:41:00 +0800

    PUTmethodSuccess

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ const fs = require('fs');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+
     // Parse the URL to get the pathname
     const { pathname } = url.parse(req.url);
-    if (req.method === 'GET') {
-        // Parse the URL to get the pathname and product ID
-        const { pathname } = url.parse(req.url);
-        const match = pathname.match(/^\/products\/(\d+)$/);
-    
+
+    // Define the match variable here to make it accessible in both GET and PUT handlers
+    const match = pathname.match(/^\/products\/(\d+)$/);
+
+    if (req.method === 'GET') {    
         if (match) {
             // Handle GET requests to /products/{id} endpoint
             const productId = parseInt(match[1], 10);
@@ -48,8 +49,62 @@ const server = http.createServer((req, res) => {
                 }
             });
         }
-    } else
-    if (req.method === 'GET' && pathname === '/products') {
+    } else if (req.method === 'PUT' && match) {
+        // Handle PUT requests to /products/{id} endpoint
+        const productId = parseInt(match[1], 10);
+
+        // Read the existing products from products.json (if the file exists)
+        fs.readFile('products.json', 'utf8', (err, data) => {
+            // Rest of your PUT request handling code...
+            if (err) {
+                console.error('Error reading products.json:', err);
+                res.writeHead(500, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ error: 'Internal Server Error' }));
+            } else {
+                let products = [];
+                try {
+                    products = JSON.parse(data);
+                } catch (error) {
+                    console.error('Error parsing JSON:', error);
+                }
+    
+                // Find the product with the matching ID
+                const existingProductIndex = products.findIndex((p) => p.id === productId);
+    
+                if (existingProductIndex !== -1) {
+                    // Parse incoming data (updated product details) from the request body
+                    let requestBody = '';
+                    req.on('data', (chunk) => {
+                        requestBody += chunk.toString();
+                    });
+    
+                    req.on('end', () => {
+                        const updatedProduct = JSON.parse(requestBody);
+    
+                        // Update the product details in the products array
+                        products[existingProductIndex] = updatedProduct;
+    
+                        // Save the updated products array to products.json
+                        fs.writeFile('products.json', JSON.stringify(products, null, 2), 'utf8', (err) => {
+                            if (err) {
+                                console.error('Error writing to products.json:', err);
+                                res.writeHead(500, { 'Content-Type': 'application/json' });
+                                res.end(JSON.stringify({ error: 'Internal Server Error' }));
+                            } else {
+                                // Return the updated product with a 200 status code
+                                res.writeHead(200, { 'Content-Type': 'application/json' });
+                                res.end(JSON.stringify(updatedProduct));
+                            }
+                        });
+                    });
+                } else {
+                    // Return a 404 status code if the product is not found
+                    res.writeHead(404, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify({ error: 'Product not found' }));
+                }
+            }
+        });
+    } else if (req.method === 'GET' && pathname === '/products') {
         // Handle GET requests to /products endpoint
     
         // Read the existing products from products.json (if the file exists)
@@ -71,8 +126,7 @@ const server = http.createServer((req, res) => {
                 res.end(JSON.stringify(products));
             }
         });
-    } else
-    if (req.method === 'POST' && pathname === '/products') {
+    } else if (req.method === 'POST' && pathname === '/products') {
       // Handle POST requests to /products endpoint
       
       // Create an empty array to hold products (if not already defined)
