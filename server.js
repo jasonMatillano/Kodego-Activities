cc7bb522923b385cd42c37065c1befc0e6eb1d60
Jason <mat.lopez072023@gmail.com>
2023-10-07 08:06:01 +0800

    PUT/UPDATE reqest added

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // Import Necessary Modules
 const http = require('http');
 const url = require('url');
-const querystring = require('querystring');
 const fs = require('fs');
 
 // Create an HTTP Server at port 3000
@@ -10,13 +9,122 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     // Parse the URL to get the pathname
     const { pathname } = url.parse(req.url);
+
     if (req.method === 'GET') {
+        if (pathname === '/products') {
+            // Handle GET requests to /products endpoint
+            fs.readFile('products.json', 'utf8', (err, data) => {
+                if (err) {
+                    console.error('Error reading products.json:', err);
+                    res.writeHead(500, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify({ error: 'Internal Server Error' }));
+                } else {
+                    let products = [];
+                    try {
+                        products = JSON.parse(data);
+                    } catch (error) {
+                        console.error('Error parsing JSON:', error);
+                    }
+    
+                    // Return the list of all products with a 200 status code
+                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify(products));
+                }
+            });
+        } else {
+            const match = pathname.match(/^\/products\/(\d+)$/);
+            if (match) {
+                // Handle GET requests to /products/{id} endpoint
+                const productId = parseInt(match[1], 10);
+    
+                // Read the existing products from products.json (if the file exists)
+                fs.readFile('products.json', 'utf8', (err, data) => {
+                    if (err) {
+                        console.error('Error reading products.json:', err);
+                        res.writeHead(500, { 'Content-Type': 'application/json' });
+                        res.end(JSON.stringify({ error: 'Internal Server Error' }));
+                    } else {
+                        let products = [];
+                        try {
+                            products = JSON.parse(data);
+                        } catch (error) {
+                            console.error('Error parsing JSON:', error);
+                        }
+    
+                        // Find the product with the matching ID
+                        const product = products.find((p) => p.id === productId);
+    
+                        if (product) {
+                            // Return the product details with a 200 status code
+                            res.writeHead(200, { 'Content-Type': 'application/json' });
+                            res.end(JSON.stringify(product));
+                        } else {
+                            // Return a 404 status code if the product is not found
+                            res.writeHead(404, { 'Content-Type': 'application/json' });
+                            res.end(JSON.stringify({ error: 'Product not found' }));
+                        }
+                    }
+                });
+            } else {
+                // Handle other GET routes here
+                // ...
+            }
+        }
+    } else if (req.method === 'POST' && pathname === '/products') {
+        // Read the existing products from products.json (if the file exists)
+        fs.readFile('products.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error('Error reading products.json:', err);
+                res.writeHead(500, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ error: 'Internal Server Error' }));
+            } else {
+                let products = [];
+                try {
+                    products = JSON.parse(data);
+                } catch (error) {
+                    console.error('Error parsing JSON:', error);
+                }
+    
+                // Automatically assign an ID in ascending order
+                const nextProductId = products.length > 0 ? products[products.length - 1].id + 1 : 1;
+    
+                // Parse incoming data (product details) from the request body
+                let requestBody = '';
+                req.on('data', (chunk) => {
+                    requestBody += chunk.toString();
+                });
+    
+                req.on('end', () => {
+                    const newProduct = JSON.parse(requestBody);
+    
+                    // Assign the next available ID to the new product
+                    newProduct.id = nextProductId;
+    
+                    // Add the new product to the products array
+                    products.push(newProduct);
+    
+                    // Save the updated products array to products.json
+                    fs.writeFile('products.json', JSON.stringify(products, null, 2), 'utf8', (err) => {
+                        if (err) {
+                            console.error('Error writing to products.json:', err);
+                            res.writeHead(500, { 'Content-Type': 'application/json' });
+                            res.end(JSON.stringify({ error: 'Internal Server Error' }));
+                        } else {
+                            // Return the created product with a 201 status code
+                            res.writeHead(201, { 'Content-Type': 'application/json' });
+                            res.end(JSON.stringify(newProduct));
+                        }
+                    });
+                });
+            }
+        });
+    } else if (req.method === 'PUT') {
         // Parse the URL to get the pathname and product ID
         const { pathname } = url.parse(req.url);
         const match = pathname.match(/^\/products\/(\d+)$/);
     
         if (match) {
-            // Handle GET requests to /products/{id} endpoint
+            // Handle PUT requests to /products/{id} endpoint
             const productId = parseInt(match[1], 10);
     
             // Read the existing products from products.json (if the file exists)
@@ -34,12 +142,34 @@ const server = http.createServer((req, res) => {
                     }
     
                     // Find the product with the matching ID
-                    const product = products.find((p) => p.id === productId);
+                    const existingProductIndex = products.findIndex((p) => p.id === productId);
     
-                    if (product) {
-                        // Return the product details with a 200 status code
-                        res.writeHead(200, { 'Content-Type': 'application/json' });
-                        res.end(JSON.stringify(product));
+                    if (existingProductIndex !== -1) {
+                        // Parse incoming data (updated product details) from the request body
+                        let requestBody = '';
+                        req.on('data', (chunk) => {
+                            requestBody += chunk.toString();
+                        });
+    
+                        req.on('end', () => {
+                            const updatedProduct = JSON.parse(requestBody);
+    
+                            // Update the product details in the products array
+                            products[existingProductIndex] = updatedProduct;
+    
+                            // Save the updated products array to products.json
+                            fs.writeFile('products.json', JSON.stringify(products, null, 2), 'utf8', (err) => {
+                                if (err) {
+                                    console.error('Error writing to products.json:', err);
+                                    res.writeHead(500, { 'Content-Type': 'application/json' });
+                                    res.end(JSON.stringify({ error: 'Internal Server Error' }));
+                                } else {
+                                    // Return the updated product with a 200 status code
+                                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                                    res.end(JSON.stringify(updatedProduct));
+                                }
+                            });
+                        });
                     } else {
                         // Return a 404 status code if the product is not found
                         res.writeHead(404, { 'Content-Type': 'application/json' });
@@ -48,78 +178,13 @@ const server = http.createServer((req, res) => {
                 }
             });
         }
-    } else
-    if (req.method === 'GET' && pathname === '/products') {
-        // Handle GET requests to /products endpoint
-    
-        // Read the existing products from products.json (if the file exists)
-        fs.readFile('products.json', 'utf8', (err, data) => {
-            if (err) {
-                console.error('Error reading products.json:', err);
-                res.writeHead(500, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ error: 'Internal Server Error' }));
-            } else {
-                let products = [];
-                try {
-                    products = JSON.parse(data);
-                } catch (error) {
-                    console.error('Error parsing JSON:', error);
-                }
-    
-                // Return the list of all products with a 200 status code
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify(products));
-            }
-        });
-    } else
-    if (req.method === 'POST' && pathname === '/products') {
-      // Handle POST requests to /products endpoint
-      
-      // Create an empty array to hold products (if not already defined)
-      let products = [];
-  
-      // Read the existing products from products.json (if the file exists)
-      fs.readFile('products.json', 'utf8', (err, data) => {
-        if (!err) {
-          try {
-            products = JSON.parse(data);
-          } catch (error) {
-            console.error('Error parsing JSON:', error);
-          }
-        }
-  
-        // Parse incoming data (product details) from the request body
-        let requestBody = '';
-        req.on('data', (chunk) => {
-          requestBody += chunk.toString();
-        });
-  
-        req.on('end', () => {
-          const newProduct = JSON.parse(requestBody);
-  
-          // Add the new product to the products array
-          products.push(newProduct);
-  
-          // Save the updated products array to products.json
-          fs.writeFile('products.json', JSON.stringify(products, null, 2), 'utf8', (err) => {
-            if (err) {
-              console.error('Error writing to products.json:', err);
-              res.writeHead(500, { 'Content-Type': 'application/json' });
-              res.end(JSON.stringify({ error: 'Internal Server Error' }));
-            } else {
-              // Return the created product with a 201 status code
-              res.writeHead(201, { 'Content-Type': 'application/json' });
-              res.end(JSON.stringify(newProduct));
-            }
-          });
-        });
-      });
-    } else {
-      // Handle other routes and methods here
-      // ...
+    }    
+    else {
+        // Handle other routes and methods here
+        // ...
     }
-  });
+});
 
 server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}/`);
+    console.log(`Server is running on http://localhost:${port}/`);
 });
