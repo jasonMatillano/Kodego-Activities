8d0d25450ec182f5515219396985234f09ca44e3
Jason <mat.lopez072023@gmail.com>
2023-10-21 22:57:31 +0800

    added description

--- routers/users.js
+++ routers/users.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-
+// Route for displaying a user list when accessing '/'
 router.get('/', (req, res) => {
+    // This route handles the root URL ('/') and sends a response of "User List".
     res.send("User List");
-})
+});
 
+// Route for displaying a new user form when accessing '/new'
 router.get('/new', (req, res) => {
+    // This route handles the '/new' URL and sends a response of "User New Form".
     res.send("User New Form");
-})
+});
 
 module.exports = router;
