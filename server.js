40b257aa36360550561bd9e65c132447589fe845
Jason <mat.lopez072023@gmail.com>
2023-10-21 22:54:15 +0800

    user router working

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs'); // Corrected typo
+
 app.get('/', (req, res) => {
-    // res.send('Hello World!');
-    res.sendStatus(400);
+    console.log('here');  
+    res.render('index',{ text : " <h2>Hello World2</h2>"});
 });
 
-app.listen(3000)
+const userRouter = require('./routers/users');
+
+app.use('/users', userRouter);
+
+
+app.listen(3000);
