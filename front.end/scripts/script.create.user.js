c7e6e3316e6c590516efee7a06f49b5266c4a466
Jason <mat.lopez072023@gmail.com>
2023-10-22 17:42:57 +0800

    added create usr

--- front.end/scripts/script.create.user.js
+++ front.end/scripts/script.create.user.js
@@ -2,19 +2,16 @@
 const addUserButton = document.getElementById("add-user");
 addUserButton.addEventListener("click", function () {
     // Retrieve the values from the input fields
-    const idInput = document.getElementById("id-input");
     const usernameInput = document.getElementById("username-input");
     const passwordInput = document.getElementById("password-input");
     const emailInput = document.getElementById("email-input");
 
-    const id = idInput.value;
     const username = usernameInput.value;
     const password = passwordInput.value;
     const email = emailInput.value;
 
     // Create an object with the input values
     const userObject = {
-        id: id,
         username: username,
         password: password,
         email: email
@@ -24,8 +21,29 @@ addUserButton.addEventListener("click", function () {
     console.log("User Object:", userObject);
 
     // Clear the input fields
-    idInput.value = "";
     usernameInput.value = "";
     passwordInput.value = "";
     emailInput.value = "";
+
+    // Send a POST request to add the user
+    fetch('http://localhost:3030/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userObject)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to add user');
+        }
+        return response.json();
+    })
+    .then(data => {
+        // Handle the response if needed
+        console.log('User added successfully:', data);
+    })
+    .catch(error => {
+        console.error('Error adding user:', error);
+    });
 });
