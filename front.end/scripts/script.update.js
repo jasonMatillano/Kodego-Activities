66bd9bce965ec33dcee28dd91d9ab1adce29a24d
Jason <mat.lopez072023@gmail.com>
2023-10-25 12:08:59 +0800

    remove update btn from create user && add labels in modal

--- front.end/scripts/script.update.js
+++
@@ -1,5 +0,0 @@
-// Add a click event listener to the "Update Now" button
-document.getElementById("update-user").addEventListener("click", () => {
-    // Open the modal
-    $('#updateUserModal').modal('show');
-});
