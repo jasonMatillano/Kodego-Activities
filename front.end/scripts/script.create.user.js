de32fb1b0352dbed771ab4939e3480fb4f42f7ac
Jason <mat.lopez072023@gmail.com>
2023-10-25 11:10:25 +0800

    added toggle to add user

--- front.end/scripts/script.create.user.js
+++ front.end/scripts/script.create.user.js
@@ -47,3 +47,17 @@ addUserButton.addEventListener("click", function () {
         console.error('Error adding user:', error);
     });
 });
+
+
+    // Add an event listener to the "Add User" button
+    const toggleUserButton = document.getElementById("toggle-add-user");
+    const addUserForm = document.getElementById("add-user-form");
+
+    toggleUserButton.addEventListener("click", function () {
+        // Toggle the visibility of the add user form
+        if (addUserForm.style.display === "block" || addUserForm.style.display === "") {
+            addUserForm.style.display = "none"; // Hide the form
+        } else {
+            addUserForm.style.display = "block"; // Show the form
+        }
+    });
